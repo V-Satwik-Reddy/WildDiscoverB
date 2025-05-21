21854c89ab722a6ed4ba1b5b9a32dd7861ca828c
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-21 22:12:14 +0530

    user models start

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/',(req,res)=>{
     res.json("Hello World");
 })
 
+app.use("/user",require("./routes/user.js"));
 app.listen(3000,()=>{
     console.log("Sever running of http://localhost:3000");
 })
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const router=express.Router();
+const mongoose=require('mongoose');
+
+
+router.get('/',(req,res)=>{
+    res.json("Hello from user");
+})
+
+module.exports =router;
