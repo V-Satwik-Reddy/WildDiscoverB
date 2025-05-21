032ee5bc8ad321f1192cb37b6bd911e87ba2aa5f
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-21 22:37:41 +0530

    updated user model

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema=new mongoose.Schema({
     name:{type:String,required:true},
-    UID:{
+    phone:{
         type:Number,
         required:true,
         unique:true,
