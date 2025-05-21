7e9ae7b580677141add3a6350d3421f79054f8c2
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-21 22:27:38 +0530

    connection to mongodb success

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
+const morgan=require('morgan');
+const dotenv=require('dotenv');
+dotenv.config();
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=>console.log("MongoDB connected"))
+    .catch((err)=>console.log(err));
 
 app.get('/',(req,res)=>{
     res.json("Hello World");
 })
+
 
 app.use("/user",require("./routes/user.js"));
 app.listen(3000,()=>{
